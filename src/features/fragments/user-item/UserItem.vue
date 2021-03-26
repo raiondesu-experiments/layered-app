b1b1eac41e603893bf66c3d6772d9f714b32e257
kaskar2008 <dev.kaskar2008@gmail.com>
2021-03-26 05:58:13 +0300

    Add user-item fragment

---
+++ src/features/fragments/user-item/UserItem.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+li.user-item {{ user.firstName }}
+</template>
+
+<script lang="ts">
+import { IUser } from '@/domain/user/resolvers';
+import { defineComponent, PropType, toRefs } from 'vue';
+
+export default defineComponent({
+  name: 'UserItem',
+  props: {
+    user: {
+      type: Object as PropType<IUser>,
+      required: true,
+    },
+  },
+  setup(props) {
+    const { user } = toRefs(props);
+
+    return {
+      user,
+    };
+  },
+})
+</script>
+
+<style lang="stylus" scoped>
+.user-item
+  cursor pointer
+</style>
