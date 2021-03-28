29e285b4bca3ad9f34700a112a9bb0bdc4a44804
kaskar2008 <dev.kaskar2008@gmail.com>
2021-03-28 18:01:42 +0300

    Add [router] main entry

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from '@/features/app/App.vue'
+import { router } from '@/domain/routing/business';
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app');
