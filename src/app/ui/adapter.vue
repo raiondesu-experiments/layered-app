fdcd86024ddd35560606889b6f6ecfa47fc4d3d4
Raiondesu <a.raiondesu@gmail.com>
2021-03-28 23:10:18 +0300

    Refactor: move layout to shared/ui

--- src/app/ui/adapter.vue
+++ src/app/ui/adapter.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { useLayout } from '@/_shared/layout/features';
+import { useLayout } from '@/_shared/ui/layout';
 import { layoutMap, DEFAULT_LAYOUT } from './layouts';
 
 export default defineComponent({
